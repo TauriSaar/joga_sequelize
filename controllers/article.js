824379ebfc02d66f4ec06395e94052e53ea26065
Tauri <tauri.saar@voco.ee>
2023-03-07 17:25:21 +0200

    Getted article author data

--- controllers/article.js
+++ controllers/article.js
@@ -1,13 +1,11 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('mysql://root:qwerty@localhost:3306/joga_sequelize');
-
-
 const models = require('../models');
 const getAllArticles = (req, res) => {
     models.Article.findAll()
         .then(articles => {
             console.log(articles);
-            return res.status(200).json( {articles} );
+            return res.status(200).json( {articles});
         })
         .catch(err => {
             return res.status(500).send(err.message);
@@ -17,21 +15,42 @@ const getArticleBySlug = (req, res) => {
     models.Article.findOne({
             where: {
                 slug: req.params.slug
+            },
+            include: [{
+                model: models.Author,
         },
-        include: [{
-        model: models.Author,
-    }],
+        {
+            model: models.Tags,
+            through: {
+                model: models.ArticleTags
+            }
+        }
+],
 })
 .then(article => {
     console.log(article)
-    return res.status(200).json( {article} );
+    return res.status(200).json({article});
 })
     .catch(error => {
         return res.status(500).send(error.message);
     })
 }
-
+const getArticleByAuthor = (req,res) =>{
+    models.Article.findAll({
+        where: {
+            author_id:req.params.author_id
+        },
+    })
+        .then(article =>{
+            console.log(article)
+            return res.status(200).json({article});
+        })
+        .catch(error=>{
+            return res.status(500).send(error.message);
+        })
+}
 module.exports = {
     getAllArticles,
-    getArticleBySlug
+    getArticleBySlug,
+    getArticleByAuthor
 }
